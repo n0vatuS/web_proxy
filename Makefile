937bf00c88fa16fa3cb57985f5105811321ac663
Taeho Lee <adldotori@gmail.com>
2019-12-22 02:48:48 +0900

    Proxying Complete

--- Makefile
+++ Makefile
@@ -1,13 +1,7 @@
 all: web_proxy
 
-web_proxy: main.o utils.o
-	g++ -std=c++11 -o web_proxy main.o utils.o -pthread
-
-main.o: main.cpp
-	g++ -std=c++11 -c -o main.o main.cpp -pthread
-
-utils.o: utils.cpp
-	g++ -std=c++11 -c -o utils.o utils.cpp -pthread
+web_proxy: main.cpp
+	g++ -std=c++11 -o web_proxy main.cpp -pthread
 
 clean:
 	rm -f *.o web_proxy
--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h> // for socket
 #include <stdlib.h> // for exit
 #include <pthread.h>
+#include <netdb.h>
 
 #include <set>
 #include <vector>
@@ -14,6 +15,15 @@
 using namespace std;
 
 set<int> Clients;
+
+bool compare_method(char * packet) {
+	const char * method[6] = {"GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS"};
+	for(int i = 0; i < 6; i++) {
+		if(!memcmp(packet, method[i], strlen(method[i])) && packet[strlen(method[i])] == 0x20)
+            return true;
+	}
+	return false;
+}
 
 void echo(int childfd) {
 	if (childfd < 0) {
@@ -32,15 +42,51 @@ void echo(int childfd) {
 			Clients.erase(childfd);
 			break;
 		}
-		snprintf(buf+received, 14, "  [Client %d]\0", childfd);
 		printf("%s\n", buf);
-        
-        ssize_t sent = send(childfd, buf, strlen(buf), 0);
-        if (sent == 0) {
-            printf("send failed to %d", childfd);
-            Clients.erase(childfd);
+        if(!compare_method(buf)) {
             break;
         }
+        char host[50];
+        const char * str = "Host: ";
+        char * tmp = strstr((char *)buf, str);
+        if(tmp == NULL) {
+            return;
+        }
+        int i = 0;
+        while(tmp[i + strlen(str)] != 0x0d) {
+            host[i] = tmp[i+strlen(str)];
+            i++;
+        }
+        printf("host : %s\n", host);
+
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(80);
+        memset(addr.sin_zero, 0, sizeof(addr.sin_zero));
+        struct hostent * host_entry = gethostbyname(host); 
+        char * ip = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+        if(!inet_aton(ip, &addr.sin_addr)) {
+            printf("Conversion Error!\n");
+            exit(1);
+        }
+        int res = connect(sockfd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(struct sockaddr));
+        if(res < 0) {
+            printf("can't connect\n");
+            return;
+        }
+
+        ssize_t sent = send(sockfd, buf, strlen(buf), 0);
+        char buf2[BUFSIZE];
+
+		ssize_t received2 = recv(sockfd, buf2, BUFSIZE - 1, 0);
+		if (received2 == 0 || received2 == -1) {
+			break;
+		}
+		printf("%s\n", buf2);
+        
+        ssize_t sent2 = send(childfd, buf2, strlen(buf2), 0);
 	}
 }
 
